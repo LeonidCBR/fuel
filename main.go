46e6ee755a1f0f525b3f1c8e9750b87c042ce0a2
Leonid Latyshev <forlemonster@gmail.com>
2019-10-02 15:09:12 +1000

    fix json

--- main.go
+++ main.go
@@ -29,13 +29,13 @@ type configuration struct {
 	DB          string
 }
 
-type vehicle struct {
-	id    int
-	model string
-	kind  string
+type Vehicle struct {
+	ID    int    `json:"id"`
+	Model string `json:"model"`
+	Kind  string `json:"type"`
 }
 
-type vehicleJSON map[string]interface{}
+//type vehicleJSON map[string]interface{}
 
 var config configuration
 var db *sql.DB
@@ -112,65 +112,51 @@ func vehiclesIndex(w http.ResponseWriter, r *http.Request) {
 	}
 	defer rows.Close()
 
-	//vehicles := make([]*vehicle, 0)
-	vehicles := make([]vehicle, 0)
-	vehiclesJSON := make([]vehicleJSON, 0)
+	vehicles := make([]*Vehicle, 0)
+	//vehicles := make([]vehicle, 0)
+	//vehiclesJSON := make([]vehicleJSON, 0)
 	for rows.Next() {
-		//veh := new(vehicle)
-		var veh vehicle
-		var id, model, kind string
-		err := rows.Scan(&id, &model, &kind)
-		vehJSON := make(vehicleJSON)
-		vehJSON["id"] = id
-		vehJSON["type"] = kind
-		vehJSON["model"] = model
-		//err := rows.Scan(&veh.id, &veh.model, &veh.kind)
+		vehicle := new(Vehicle)
+		//var veh vehicle
+		//var id, model, kind string
+		//err := rows.Scan(&id, &model, &kind)
+		//vehJSON := make(vehicleJSON)
+		//vehJSON["id"] = id
+		//vehJSON["type"] = kind
+		//vehJSON["model"] = model
+		err := rows.Scan(&vehicle.ID, &vehicle.Model, &vehicle.Kind)
 		if err != nil {
 			http.Error(w, http.StatusText(500), 500)
 			return
 		}
-		vehicles = append(vehicles, veh)
-		vehiclesJSON = append(vehiclesJSON, vehJSON)
+		vehicles = append(vehicles, vehicle)
+		//vehiclesJSON = append(vehiclesJSON, vehJSON)
 	}
 	if err = rows.Err(); err != nil {
 		http.Error(w, http.StatusText(500), 500)
 		return
 	}
 
-	//b, err := json.Marshal(vehicles)
-	//fmt.Fprint(w, b)
-	// String(b)
+	//	println("marshaling...")
+	//	b, err := json.Marshal(vehiclesJSON)
+	//	//err = json.NewEncoder(w).Encode(&t)
 
-	/*
-		vvv := make(map[string]interface{})
-		vvv["id"] = "1"
-		vvv["type"] = "car"
-		vvv["model"] = "nissan"
-	*/
-	/*
-		for _, veh := range vehicles {
-			//!!! fmt.Fprintf(w, "%d. %s (%s)\n", veh.id, veh.model, veh.kind)
-			fmt.Printf("%d. %s (%s)\n", veh.id, veh.model, veh.kind)
-			//json.NewEncoder(w).Encode(veh)
-		}
-
-
-		type fruits map[string]int
-		fr := make(map[string]int)
-		fr["apples"] = 25
-		fr["oranges"] = 10
-	*/
-
-	println("marshaling...")
-	b, err := json.Marshal(vehiclesJSON)
-	//err = json.NewEncoder(w).Encode(&t)
+	result, err := json.Marshal(vehicles)
 	if err != nil {
 		http.Error(w, http.StatusText(500), 500)
 		return
 	}
-	os.Stdout.Write(b)
-	w.Write(b)
 
-	//fmt.Fprintf(w, `{"Name":"Alice","Body":"Hello","Time":1294706395881547000}`)
-
+	/*
+		vehicle := &Vehicle{
+			Id:    55,
+			Model: "infinity fx",
+			Kind:  "car"}
+		data, err := json.Marshal(vehicle)
+		if err != nil {
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+	*/
+	w.Write(result)
 }
