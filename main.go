a78d9749d4aac9dc8fba6b6d50ef904510855754
Leonid Latyshev <forlemonster@gmail.com>
2019-10-02 14:22:43 +1000

    add json

--- main.go
+++ main.go
@@ -35,6 +35,8 @@ type vehicle struct {
 	kind  string
 }
 
+type vehicleJSON map[string]interface{}
+
 var config configuration
 var db *sql.DB
 
@@ -92,35 +94,83 @@ func main() {
 
 func vehiclesIndex(w http.ResponseWriter, r *http.Request) {
 
+	const queryVehicles = "SELECT id, model, type FROM vehicles"
+
+	// Set headers to json
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
 	if r.Method != "GET" {
 		http.Error(w, http.StatusText(405), 405)
 		return
 	}
 
-	rows, err := db.Query("SELECT id, model, type FROM vehicles")
+	rows, err := db.Query(queryVehicles)
 	if err != nil {
 		http.Error(w, http.StatusText(500), 500)
 		return
 	}
 	defer rows.Close()
 
-	vehicles := make([]*vehicle, 0)
+	//vehicles := make([]*vehicle, 0)
+	vehicles := make([]vehicle, 0)
+	vehiclesJSON := make([]vehicleJSON, 0)
 	for rows.Next() {
-		veh := new(vehicle)
-		err := rows.Scan(&veh.id, &veh.model, &veh.kind)
+		//veh := new(vehicle)
+		var veh vehicle
+		var id, model, kind string
+		err := rows.Scan(&id, &model, &kind)
+		vehJSON := make(vehicleJSON)
+		vehJSON["id"] = id
+		vehJSON["type"] = kind
+		vehJSON["model"] = model
+		//err := rows.Scan(&veh.id, &veh.model, &veh.kind)
 		if err != nil {
 			http.Error(w, http.StatusText(500), 500)
 			return
 		}
 		vehicles = append(vehicles, veh)
+		vehiclesJSON = append(vehiclesJSON, vehJSON)
 	}
 	if err = rows.Err(); err != nil {
 		http.Error(w, http.StatusText(500), 500)
 		return
 	}
 
-	for _, veh := range vehicles {
-		fmt.Fprintf(w, "%d. %s (%s)\n", veh.id, veh.model, veh.kind)
+	//b, err := json.Marshal(vehicles)
+	//fmt.Fprint(w, b)
+	// String(b)
+
+	/*
+		vvv := make(map[string]interface{})
+		vvv["id"] = "1"
+		vvv["type"] = "car"
+		vvv["model"] = "nissan"
+	*/
+	/*
+		for _, veh := range vehicles {
+			//!!! fmt.Fprintf(w, "%d. %s (%s)\n", veh.id, veh.model, veh.kind)
+			fmt.Printf("%d. %s (%s)\n", veh.id, veh.model, veh.kind)
+			//json.NewEncoder(w).Encode(veh)
+		}
+
+
+		type fruits map[string]int
+		fr := make(map[string]int)
+		fr["apples"] = 25
+		fr["oranges"] = 10
+	*/
+
+	println("marshaling...")
+	b, err := json.Marshal(vehiclesJSON)
+	//err = json.NewEncoder(w).Encode(&t)
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
 	}
+	os.Stdout.Write(b)
+	w.Write(b)
+
+	//fmt.Fprintf(w, `{"Name":"Alice","Body":"Hello","Time":1294706395881547000}`)
 
 }
